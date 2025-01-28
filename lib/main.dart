47beb436b7e4d584314e1983f1f53b1b87937952
Nadun Charuka <nadun.charuka2016@gmail.com>
2025-01-28 14:29:07 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:notekeeperapp/screens/note_list.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+      ),
+      title: 'Flutter Demo',
+      home: const NoteList(),
+    );
+  }
+}
