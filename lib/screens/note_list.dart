c646348dc08c9f68158043723362304662097fcf
Nadun Charuka <nadun.charuka2016@gmail.com>
2025-02-20 10:45:26 +0530

    note list page done

--- lib/screens/note_list.dart
+++ lib/screens/note_list.dart
@@ -8,8 +8,51 @@ class NoteList extends StatefulWidget {
 }
 
 class _NoteListState extends State<NoteList> {
+  int count = 0;
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Notes"),
+      ),
+      body: getNoteListView(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          debugPrint("FAB clicked");
+        },
+        tooltip: "Add note",
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+
+  ListView getNoteListView() {
+    TextStyle? titleStyle = Theme.of(context).textTheme.headlineMedium;
+    return ListView.builder(
+      itemCount: count,
+      itemBuilder: (context, index) {
+        return Card(
+          color: Colors.white,
+          elevation: 2.0,
+          child: ListTile(
+            leading: CircleAvatar(
+              backgroundColor: Colors.yellow,
+              child: Icon(Icons.keyboard_arrow_right),
+            ),
+            title: Text(
+              "Dummy Title",
+              style: titleStyle,
+            ),
+            trailing: Icon(
+              Icons.delete,
+              color: Colors.grey,
+            ),
+            onTap: () {
+              debugPrint("ListTile Tapped");
+            },
+          ),
+        );
+      },
+    );
   }
 }
